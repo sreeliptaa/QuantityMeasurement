065e89fcb0825213210f02851d6cc2a3bc637226
sreeliptaa <sreelipta65@gmail.com>
2021-10-29 20:05:31 +0530

    Added: testcase for reference check

--- src/test/java/com/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -5,9 +5,30 @@ import org.junit.jupiter.api.Test;
 
 public class QuantityTest {
     @Test
+    public void given0Feetand0Feet_SouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assertions.assertEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0Feetand1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assertions.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
     public void given0FeetandNullFeet_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = null;
+        Assertions.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0FeetAnd1FeetFromReference_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
         Assertions.assertNotEquals(feet1, feet2);
     }
 }
