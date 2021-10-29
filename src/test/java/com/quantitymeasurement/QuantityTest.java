bed69eb05925f78b41f04868552c7871d55af98a
sreeliptaa <sreelipta65@gmail.com>
2021-10-29 20:15:34 +0530

    Added: testcase for type check

--- src/test/java/com/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -31,4 +31,11 @@ public class QuantityTest {
         Feet feet2 = new Feet(1.0);
         Assertions.assertNotEquals(feet1, feet2);
     }
+
+    @Test
+    public void givenType0Feetand1Feet_ShouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assertions.assertEquals(feet1.getClass(), feet2.getClass());
+    }
 }
