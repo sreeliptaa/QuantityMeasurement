e45f6277c4f42d0c239476c33eae0ce64ac9f5ac
sreeliptaa <sreelipta65@gmail.com>
2021-10-29 17:22:44 +0530

    Added: testcase for null check

--- src/test/java/com/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 
 public class QuantityTest {
     @Test
-    public void given0Feetand1Feet_ShouldReturnNotEqual() {
+    public void given0FeetandNullFeet_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(1.0);
+        Feet feet2 = null;
         Assertions.assertNotEquals(feet1, feet2);
     }
 }
