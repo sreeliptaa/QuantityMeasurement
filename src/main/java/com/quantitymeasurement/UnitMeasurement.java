5fc95b08eacc001e385c651e9650ee84680f0422
sreeliptaa <sreelipta65@gmail.com>
2021-10-29 16:03:21 +0530

    Added: welcome message

---
+++ src/main/java/com/quantitymeasurement/UnitMeasurement.java
@@ -0,0 +1,7 @@
+package com.quantitymeasurement;
+
+public class UnitMeasurement {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Quantity Measurement System");
+    }
+}
